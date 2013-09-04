4b241a6a7769cb2589d6c50d989b1a39cffbd354
jvialgent <gfrankpetersen@gmail.com>
2013-09-04 11:09:48 -0600

    Making the Basic.class file from class. Added Constructor and main to the file.

---
+++ classCode_9_4_2013/Basic.java
@@ -0,0 +1,17 @@
+package classCode_9_4_2013;
+
+
+
+public class Basic {
+	
+	public Basic(String title, int width, int height, int k) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args){
+		Basic app = new Basic("Title of app", 600, 400, 30);
+	}
+	
+	
+
+}
